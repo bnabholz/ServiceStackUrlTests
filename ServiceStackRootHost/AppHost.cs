e96822196d26ea4b669de17c6d4f8ffba9dd662d
bnabholz <bnabholz@restassuredsystem.com>
2014-11-06 11:00:15 -0500

    Example to show differences in ServiceStack base URLs.

---
+++ ServiceStackRootHost/AppHost.cs
@@ -0,0 +1,21 @@
+﻿using ServiceStack;
+using ServiceStackUrlTests.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiceStackRootHost
+{
+    public class AppHost : AppHostBase
+    {
+        public AppHost()
+            : base("URL Tests - path hosted", typeof(UrlService).Assembly)
+        { }
+
+        public override void Configure(Funq.Container container)
+        {
+
+        }
+    }
+}
---
+++ ServiceStackUrlTests.Services/UrlService.cs
@@ -0,0 +1,29 @@
+﻿using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServiceStackUrlTests.Services
+{
+    [Route("/baseurl", "GET")]
+    public class GetBaseUrl : IReturn<string>
+    { }
+
+    [Route("/handlerfactory", "GET")]
+    public class GetHandlerFactory : IReturn<string>
+    { }
+
+    public class UrlService : Service
+    {
+        public string Get(GetBaseUrl req)
+        {
+            return Request.GetBaseUrl();
+        }
+
+        public string Get(GetHandlerFactory req)
+        {
+            return HostConfig.Instance.HandlerFactoryPath ?? "(null)";
+        }
+    }
+}
